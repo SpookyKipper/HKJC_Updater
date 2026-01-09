b2f73397e3533f44af554f14fa0b3dcc18954a09
Spooky Kipper <88220209+SpookyKipper@users.noreply.github.com>
2026-01-09 22:43:37 +0800

    Remove debug values

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -7,48 +7,34 @@ class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SizedBox(
-        height: MediaQuery.of(context).size.height - 50,
-        child: Container(
-          decoration: BoxDecoration(
-            border: Border.all(
-              color: Colors.red, // Specify the border color
-              width: 2, // Specify the border width
-            ),
-            borderRadius: BorderRadius.circular(
-              12,
-            ), // Optional: Add rounded corners
-          ),
-          child: ListView(
-            physics: const ClampingScrollPhysics(),
-            children: [
-              Center(
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-        
-                  children: [
-                    Button(
-                      text: "查看授權許可 View Licenses",
-                      onPressed: () async {
-                        showLicensePage(
-                          context: context,
-                          applicationName: 'HKJC Updater',
-                          applicationVersion: "v0.0.4 (4)",
-                          applicationIcon: ConstrainedBox(
-                            constraints: BoxConstraints(maxWidth: 100),
-                            child: Image.asset("assets/images/icon.png"),
-                          ),
-        
-                          applicationLegalese: "© Spooky Services",
-                        );
-                      },
-                    ),
-                  ],
+      body: ListView(
+        physics: const ClampingScrollPhysics(),
+        children: [
+          Center(
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              
+              children: [
+                Button(
+                  text: "查看授權許可 View Licenses",
+                  onPressed: () async {
+                    showLicensePage(
+                      context: context,
+                      applicationName: 'HKJC Updater',
+                      applicationVersion: "v0.0.4 (4)",
+                      applicationIcon: ConstrainedBox(
+                        constraints: BoxConstraints(maxWidth: 100),
+                        child: Image.asset("assets/images/icon.png"),
+                      ),
+              
+                      applicationLegalese: "© Spooky Services",
+                    );
+                  },
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
